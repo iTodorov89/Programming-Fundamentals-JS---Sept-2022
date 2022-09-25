17eb089435246fb6ffe2d2f0911e857d31d7c703
Iskren Todorov <todorovplus@gmail.com>
2022-09-25 11:29:51 +0300

    Added lection 5.

---
+++ Exercise/introduce.js
@@ -0,0 +1,8 @@
+function introduce(name, age){
+    //let introduction = "Hello my name is " + name + " and I\'m " + age + ` years old`
+    let introduction = `Hello my name is ${name} and I'm ${age} years old`;
+    console.log(introduction);
+}
+
+introduce("Pesho", 20)
+introduce("Gosho", 21)
---
+++ Exercise/sumOfOddNumbers.js
@@ -0,0 +1,14 @@
+function totalSum(count) {
+
+    let oddNumber = 1;
+    let sum = 0;
+    for (let i = 0; i < count; i++) {
+        console.log(oddNumber);
+        sum += oddNumber;
+        oddNumber += 2;
+    }
+    console.log(`Sum: ${sum}`);
+
+}
+
+totalSum(5)
